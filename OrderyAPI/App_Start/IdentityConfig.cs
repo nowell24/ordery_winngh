c77701afa3ad183f613eaf07fc984f56f52b4378
Nowell_Hernandez <Nowell_Hernandez@DESKTOP-TIA3Q0D>
2019-08-26 04:46:56 +0800

    check identityconfig

--- OrderyAPI/App_Start/IdentityConfig.cs
+++ OrderyAPI/App_Start/IdentityConfig.cs
@@ -1,15 +1,10 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin;
-using OrderyAPI.Models;
+﻿using OrderyAPI.Models;
 
 namespace OrderyAPI
 {
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
 
-    public class ApplicationUserManager : UserManager<ApplicationUser>
+    /*public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
             : base(store)
@@ -41,5 +36,5 @@ namespace OrderyAPI
             }
             return manager;
         }
-    }
+    }*/
 }
